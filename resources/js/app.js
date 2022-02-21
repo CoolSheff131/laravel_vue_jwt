8994b3670adfbe5d3445d6b4f1d2eb4b1720bf3d
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-21 11:53:23 +0800

    настройка

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+import Vue from "vue";
+import Index from "./components/Index.vue";
+import router from "./router";
+require("./bootstrap");
+
+const app = new Vue({
+    el: "#app",
+    components: {
+        Index,
+    },
+    router,
+});
---
+++ resources/js/components/Fruit/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      list fruits
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
