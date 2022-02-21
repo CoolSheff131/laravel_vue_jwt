06494d9050c31c3f4bd43371ab204d1b49b70a5e
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-21 12:26:32 +0800

    регистрация и список фруктов

--- resources/js/components/Fruit/Index.vue
+++ resources/js/components/Fruit/Index.vue
@@ -1,12 +1,43 @@
 <template>
   <div>
-      list fruits
+      <table class="table">
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Price</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="fruit in fruits" v-bind:key="fruit.id">
+                <td>{{fruit.id}}</td>
+                <td>{{fruit.name}}</td>
+                <td>{{fruit.price}}</td>
+            </tr>
+        </tbody>
+      </table>
   </div>
 </template>
 
 <script>
 export default {
-
+name: 'Index',
+data(){
+    return{
+        fruits: null,
+    }
+},
+mounted(){
+    this.getFruits()
+},
+methods:{
+    getFruits(){
+        axios.get('/api/fruits')
+        .then( res=>{
+            this.fruits = res.data.data
+        })
+    }
+}
 }
 </script>
 
--- resources/js/components/User/Registration.vue
+++ resources/js/components/User/Registration.vue
@@ -1,15 +1,33 @@
 <template>
 <div class="w-25">
-    <input type="text" class="form-control" placeholder="name">
-    <input type="email" class="form-control" placeholder="email">
-    <input type="password" class="form-control" placeholder="password">
-    <input type="password" class="form-control" placeholder="confirm password">
-    <input type="submit" class="btn btn-primary">
+    <input v-model="name" type="text" class="form-control" placeholder="name">
+    <input v-model="email" type="email" class="form-control" placeholder="email">
+    <input v-model="password" type="password" class="form-control" placeholder="password">
+    <input v-model="password_confirmation" type="password" class="form-control" placeholder="confirm password">
+    <input @click.prevent="store" type="submit" class="btn btn-primary">
 </div>
 </template>
 
 <script>
 export default {
+    name: 'Registration',
+    data(){
+        return{
+            name: null,
+            email: null,
+            password: null,
+            password_confirmation: null,
+        }
+    },
+
+    methods:{
+        store(){
+            axios.post('/api/users',{name: this.name, email: this.email, password: this.password, password_confirmation: this.password_confirmation}).
+            then(res=>{
+                console.log(res);
+            })
+        }
+    }
 
 }
 </script>
