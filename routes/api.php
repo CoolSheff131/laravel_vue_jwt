8507e787b55306115b0ba7ad19ef7f7cc0ffd5f1
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-28 14:29:29 +0800

    защита роута

--- routes/api.php
+++ routes/api.php
@@ -27,11 +27,11 @@ Route::group([
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+    Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function () {
+        Route::get('/', 'IndexController');
+    });
 });
 
 Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
     Route::post('/', 'StoreController');
-});
-Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function () {
-    Route::get('/', 'IndexController');
 });
