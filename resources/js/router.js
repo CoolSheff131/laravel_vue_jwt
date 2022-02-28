15e33e7939e7de671def0ac17f011b4052dce867
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-28 15:45:15 +0800

    protected routes

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,7 +3,7 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
     mode: "history",
     routes: [
         {
@@ -26,5 +26,34 @@ export default new VueRouter({
             component: () => import("./components/User/Personal.vue"),
             name: "user.personal",
         },
+        {
+            path: "*",
+            component: () => import("./components/User/Personal.vue"),
+            name: "404",
+        },
     ],
 });
+
+router.beforeEach((to, from, next) => {
+    const access_token = localStorage.getItem("access_token");
+
+    if (!access_token) {
+        if (to.name === "user.login" || to.name === "user.registration") {
+            return next();
+        } else {
+            return next({
+                name: "user.login",
+            });
+        }
+    }
+
+    if (to.name !== "user.login" && access_token) {
+        return next({
+            name: "user.personal",
+        });
+    }
+
+    next();
+});
+
+export default router;
