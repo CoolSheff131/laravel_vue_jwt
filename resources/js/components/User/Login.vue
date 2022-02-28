457690d70c6831c3df55e7ec1a8df0a5af8c8d3d
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-28 14:36:04 +0800

    логин

--- resources/js/components/User/Login.vue
+++ resources/js/components/User/Login.vue
@@ -1,14 +1,29 @@
 <template>
 <div class="w-25">
-    <input type="email" class="form-control" placeholder="email">
-    <input type="password" class="form-control" placeholder="password">
-    <input type="submit" class="btn btn-primary">
+    <input v-model="email" type="email" class="form-control" placeholder="email">
+    <input v-model="password" type="password" class="form-control" placeholder="password">
+    <input @click.prevent="login" type="submit" class="btn btn-primary">
 </div>
 </template>
 
 <script>
 export default {
+    name: 'Login',
+    data(){
+        return{
+            email:null,
+            password: null,
+        }
+    },
 
+    methods:{
+        login(){
+            axios.post('/api/post/login',{email: this.email, paswsord:this.password})
+            .then( res=>{
+
+            })
+        }
+    }
 }
 </script>
 
--- routes/api.php
+++ routes/api.php
@@ -27,8 +27,11 @@ Route::group([
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
-    Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function () {
-        Route::get('/', 'IndexController');
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function () {
+            Route::get('/', 'IndexController');
+        });
     });
 });
 
